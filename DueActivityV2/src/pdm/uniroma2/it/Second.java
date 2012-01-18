cd20a8dcdf003ca64a6415f80ae61c2db8faef6b
unknown <boborosko@gmail.com>
2012-01-18 17:24:05 +0100

    Esercitazioni PDM2012

--- DueActivityV2/src/pdm/uniroma2/it/Second.java
+++ DueActivityV2/src/pdm/uniroma2/it/Second.java
@@ -14,7 +14,31 @@ public class Second extends Activity {
         label.setText(iltestoricevuto);
         Log.d("Second", "onCreate");
 	}
-        
+        public void OnStart() {
+        	super.onStart();
+        	Log.d("Second", "onStart");
+        }
+	    public void onRestart() {
+		super.onRestart();
+		Log.d("Second", "onRestart");
+	  }
+	  public void onResume() {
+		super.onResume();
+		Log.d("Second", "onRestart");
+	  }
+	  public void onPause () {
+		  super.onPause();
+		  Log.d("Second", "onPause");
+	  }
+	  public void onStop () {
+		  super.onStop();
+		  Log.d("Second", "onStop");
+	  }
+	  public void onDestroy () {
+		  super.onDestroy();
+		  Log.d("Second","onDestroy");
+	  
+	  }
 	}
 
 
