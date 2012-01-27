7332be4cfd51ef1faa091295113b857e8bc4a4ef
unknown <boborosko@gmail.com>
2012-01-27 18:31:43 +0100

    Esercitaz. Nr5/Eserc. Nr1:ToggleButton ed ImageView

---
+++ SwitchImage/src/pdm/RobertoRusco/SwitchImageActivity.java
@@ -0,0 +1,35 @@
+package pdm.RobertoRusco;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
+
+public class SwitchImageActivity extends Activity {
+	ImageView iv;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        iv = (ImageView)findViewById(R.id.imageView1);
+        
+        ToggleButton button = (ToggleButton)findViewById(R.id.toggleButton1);
+        button.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				// TODO Auto-generated method stub
+				if (isChecked) {
+					iv.setImageResource(R.drawable.button_pause);
+				}else{
+					iv.setImageResource(R.drawable.button_play);
+				}
+			}
+        });
+    }
+}
+   
