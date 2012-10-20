5f69a9a22bfaea211e662d94875fa1bbdf9f5c96
unknown <boborosko@gmail.com>
2012-10-20 12:12:29 +0200

    Esercizio1,Poesia

---
+++ Esercitazione001/src/pdm/robertorusco/Esercitazione001Activity.java
@@ -0,0 +1,13 @@
+package pdm.robertorusco;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Esercitazione001Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
